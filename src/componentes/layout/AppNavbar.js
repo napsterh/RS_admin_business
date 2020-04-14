29b51cb92e7a99f2f40a06f82852b7966998eedd
Napster <rome.1205h@gmail.com>
2020-04-14 02:04:35 -0500

    update final settings

---
+++ src/componentes/layout/AppNavbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+export default class AppNavBar extends Component {
+    render() {
+        return (
+            <div>
+        
+            </div>
+        )
+    }
+}
--- src/componentes/vistas/ListaBusiness.js
+++ src/componentes/vistas/ListaBusiness.js
@@ -5,7 +5,7 @@ class ListaBusiness extends Component {
     render() {
         return (
             <div>
-                <Button variant="contained" color="primary">Color primario</Button>
+                <Button variant="contained" color="primary">Seleccionar negocio</Button>
                 <Button variant="contained" color="secondary">Color secundario</Button>
             </div>
         )
