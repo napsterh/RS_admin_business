2a5ae30ec0b7aeddf6c9f40a486a10c4d41ccd41
Napster <rome.1205h@gmail.com>
2020-04-16 23:54:05 -0500

    creando sesion de un usuario desde firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import './App.css';
 import ListaBusiness from './componentes/vistas/ListaBusiness';
@@ -8,26 +8,38 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import theme from './theme/theme';
 import RegistrarUser from './componentes/seguridad/RegistrarUser';
 import Login from './componentes/seguridad/Login';
+import { FirebaseContext } from './server';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <MuiThemeProvider theme={theme}>
-          <AppNavBar />
 
-          <Grid container>
-            <Switch>
-              <Route path="/" exact component={ListaBusiness}></Route>
-              <Route path="/auth/Login" exact component={Login}></Route>
-              <Route path="/auth/RegistrarUser" exact component={RegistrarUser}></Route>
-            </Switch>
-          </Grid>
+function App(props) {
+  let firebase = React.useContext(FirebaseContext);
+  const [autenticacionIniciada, setupFirebaseInicial] = React.useState(false);
 
-        </MuiThemeProvider>
-      </BrowserRouter>
-    )
-  }
+
+  useEffect(() =>{
+     firebase.estaIniciado().then(val=>{
+       setupFirebaseInicial(val);
+     })
+  })
+
+  return autenticacionIniciada !== false ? (
+    <BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <AppNavBar />
+
+        <Grid container>
+          <Switch>
+            <Route path="/" exact component={ListaBusiness}></Route>
+            <Route path="/auth/login" exact component={Login}></Route>
+            <Route path="/auth/RegistrarUser" exact component={RegistrarUser}></Route>
+          </Switch>
+        </Grid>
+
+      </MuiThemeProvider>
+    </BrowserRouter>
+  )
+  : null
+  ;
 }
 
 export default App;
