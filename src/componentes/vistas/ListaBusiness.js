5f0804c730888f19319f6989074b1cbd9158bc58
Napster <rome.1205h@gmail.com>
2020-04-30 04:11:22 -0500

    grid de lista de negocios

--- src/componentes/vistas/ListaBusiness.js
+++ src/componentes/vistas/ListaBusiness.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import { Container, Paper, Grid, Breadcrumbs, Link, Typography, TextField } from '@material-ui/core';
+import { Container, Paper, Grid, Breadcrumbs, Link, Typography, TextField, Card, CardContent, CardActions } from '@material-ui/core';
+import CardMedia from "@material-ui/core/CardMedia";
 import HomeIcon from '@material-ui/icons/Home';
 import { consumerFirebase } from '../../server';
+import logo from '../../logo.svg';
 
 const style = {
     cardGrid: {
@@ -19,11 +21,53 @@ const style = {
     },
     gridTextfield: {
         marginTop: "20px"
+    },
+    card: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    cardMedia: {
+        paddingTop: "56.25%"
+    },
+    cardContent: {
+        flexGrow: 1
     }
 }
 
 
 class ListaBusiness extends Component {
+
+    state = {
+        negocios: [],
+        textoBusqueda: ""
+    }
+
+    cambiarBusquedaTexto = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    async componentDidMount(){
+        let objectQuery = this.props.firebase.db.collection("Business").orderBy("direccion");
+
+        const snapshot = await objectQuery.get();
+
+        const arrayNegocio = snapshot.docs.map(doc => {
+            let data = doc.data();
+            console.log(data);
+            let id = doc.id;
+            return {id, ...data};
+        })
+
+        this.setState({
+            negocios: arrayNegocio
+        })
+
+
+    }
+
     render() {
         return (
                 <Container style={style.cardGrid}>
@@ -46,12 +90,56 @@ class ListaBusiness extends Component {
                                 name="textoBusqueda"
                                 variant="outlined"
                                 label="Ingrese el negocio a buscar"
+                                onChange = {this.cambiarBusquedaTexto}
+                                value={this.state.textoBusqueda}
                             />
                         </Grid>
+
+                        <Grid item xs={12} sm={12} style={style.gridTextfield}>
+                            <Grid container spacing={4}>
+                                {this.state.negocios.map(card => (
+                                    <Grid item key={card.id} xs={12} sm={6} md={4}>
+                                        <Card style={style.card}>
+                                            <CardMedia
+                                                style={style.cardMedia}
+                                                image={
+                                                    card.fotos
+                                                    ? card.fotos[0]
+                                                        ? card.fotos[0]
+                                                        : logo
+                                                    : logo
+                                                }
+                                                title="Mi negocio"
+                                            />
+                                            <CardContent style={style.cardContent}>
+                                                <Typography gutterBottom variant="h5" component="h2">
+                                                    {card.distrito + ", " + card.ciudad}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions>
+                                                <Button
+                                                    size="small"
+                                                    color="secondary"
+                                                >
+                                                    Editar
+                                                </Button>
+                                                <Button
+                                                    size="small"
+                                                    color="secondary"
+                                                >
+                                                    Eliminar
+                                                </Button>
+                                            </CardActions>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
+
                     </Paper>
                 </Container>
         );
-    }
-}
+    };
+};
 
 export default consumerFirebase(ListaBusiness);
