e1210f1189ef48a7c772f9bcd4beb2b29916b931
Napster <rome.1205h@gmail.com>
2020-05-01 00:12:19 -0500

    funcion eliminar elementos de firebase

--- src/componentes/vistas/ListaBusiness.js
+++ src/componentes/vistas/ListaBusiness.js
@@ -46,26 +46,26 @@ class ListaBusiness extends Component {
     cambiarBusquedaTexto = e => {
         const self = this;
         self.setState({
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
 
-        if(self.state.typingTimeout){
+        if (self.state.typingTimeout) {
             clearTimeout(self.state.typingTimeout);
         }
 
         self.setState({
-            name : e.target.value,
-            typing : false,
-            typingTimeout : setTimeout(goTime => {
+            name: e.target.value,
+            typing: false,
+            typingTimeout: setTimeout(goTime => {
                 let objectQuery = this.props.firebase.db
-                .collection("Business")
-                .orderBy("distrito")
-                .where("keywords", "array-contains", self.state.textoBusqueda.toLowerCase());
-
-                if(self.state.textoBusqueda.trim() === ""){
-                    objectQuery = this.props.firebase.db
                     .collection("Business")
                     .orderBy("distrito")
+                    .where("keywords", "array-contains", self.state.textoBusqueda.toLowerCase());
+
+                if (self.state.textoBusqueda.trim() === "") {
+                    objectQuery = this.props.firebase.db
+                        .collection("Business")
+                        .orderBy("distrito")
                 }
 
 
@@ -73,11 +73,11 @@ class ListaBusiness extends Component {
                     const arrayNegocio = snapshot.docs.map(doc => {
                         let data = doc.data();
                         let id = doc.id;
-                        return {id, ...data};
+                        return { id, ...data };
                     })
 
                     this.setState({
-                        negocios : arrayNegocio
+                        negocios: arrayNegocio
                     })
                 })
 
@@ -85,7 +85,7 @@ class ListaBusiness extends Component {
         })
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         let objectQuery = this.props.firebase.db.collection("Business").orderBy("direccion");
 
         const snapshot = await objectQuery.get();
@@ -94,86 +94,104 @@ class ListaBusiness extends Component {
             let data = doc.data();
             console.log(data);
             let id = doc.id;
-            return {id, ...data};
+            return { id, ...data };
         })
 
         this.setState({
             negocios: arrayNegocio
+        });
+    }
+
+    eliminarNegocio = id => {
+        this.props.firebase.db
+            .collection("Business")
+            .doc(id)
+            .delete()
+            .then(success => {
+                this.eliminarNegocioListaEstado(id);
+            })
+    }
+
+    eliminarNegocioListaEstado = id => {
+        const negocioListaNueva = this.state.negocios.filter(
+            negocio => negocio.id !== id
+        )
+        this.setState({
+            negocios: negocioListaNueva
         })
-
-
     }
 
     render() {
         return (
-                <Container style={style.cardGrid}>
-                    <Paper style={style.paper}>
-                        <Grid item xs={12} sm={12}>
-                            <Breadcrumbs aria-label="breadcrumbs">
-                                <Link color="inherit" style={style.link} href="/">
-                                    <HomeIcon/>
+            <Container style={style.cardGrid}>
+                <Paper style={style.paper}>
+                    <Grid item xs={12} sm={12}>
+                        <Breadcrumbs aria-label="breadcrumbs">
+                            <Link color="inherit" style={style.link} href="/">
+                                <HomeIcon />
                                     Home
                                 </Link>
-                                <Typography color="textPrimary">Mis Negocios</Typography>
-                            </Breadcrumbs>
-                        </Grid>
-                        <Grid item xs={12} sm={6} style={style.gridTextfield}>
-                            <TextField
-                                fullWidth
-                                InputLabelProps = {{
-                                    shrink : true
-                                }}
-                                name="textoBusqueda"
-                                variant="outlined"
-                                label="Ingrese el negocio a buscar"
-                                onChange = {this.cambiarBusquedaTexto}
-                                value={this.state.textoBusqueda}
-                            />
-                        </Grid>
+                            <Typography color="textPrimary">Mis Negocios</Typography>
+                        </Breadcrumbs>
+                    </Grid>
+                    <Grid item xs={12} sm={6} style={style.gridTextfield}>
+                        <TextField
+                            fullWidth
+                            InputLabelProps={{
+                                shrink: true
+                            }}
+                            name="textoBusqueda"
+                            variant="outlined"
+                            label="Ingrese el negocio a buscar"
+                            onChange={this.cambiarBusquedaTexto}
+                            value={this.state.textoBusqueda}
+                        />
+                    </Grid>
 
-                        <Grid item xs={12} sm={12} style={style.gridTextfield}>
-                            <Grid container spacing={4}>
-                                {this.state.negocios.map(card => (
-                                    <Grid item key={card.id} xs={12} sm={6} md={4}>
-                                        <Card style={style.card}>
-                                            <CardMedia
-                                                style={style.cardMedia}
-                                                image={
-                                                    card.fotos
+                    <Grid item xs={12} sm={12} style={style.gridTextfield}>
+                        <Grid container spacing={4}>
+                            {this.state.negocios.map(card => (
+                                <Grid item key={card.id} xs={12} sm={6} md={4}>
+                                    <Card style={style.card}>
+                                        <CardMedia
+                                            style={style.cardMedia}
+                                            image={
+                                                card.fotos
                                                     ? card.fotos[0]
                                                         ? card.fotos[0]
                                                         : logo
                                                     : logo
-                                                }
-                                                title="Mi negocio"
-                                            />
-                                            <CardContent style={style.cardContent}>
-                                                <Typography gutterBottom variant="h5" component="h2">
-                                                    {card.distrito + ", " + card.ciudad}
-                                                </Typography>
-                                            </CardContent>
-                                            <CardActions>
-                                                <Button
-                                                    size="small"
-                                                    color="secondary"
-                                                >
-                                                    Editar
+                                            }
+                                            title="Mi negocio"
+                                        />
+                                        <CardContent style={style.cardContent}>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {card.distrito + ", " + card.ciudad}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button
+                                                size="small"
+                                                color="secondary"
+                                            >
+                                                Editar
                                                 </Button>
-                                                <Button
-                                                    size="small"
-                                                    color="secondary"
-                                                >
-                                                    Eliminar
+                                            <Button
+                                                size="small"
+                                                color="secondary"
+                                                onClick={() => this.eliminarNegocio(card.id)}
+                                            >
+                                                Eliminar
                                                 </Button>
-                                            </CardActions>
-                                        </Card>
-                                    </Grid>
-                                ))}
-                            </Grid>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
                         </Grid>
+                    </Grid>
 
-                    </Paper>
-                </Container>
+                </Paper>
+            </Container>
         );
     };
 };
